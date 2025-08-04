f7cee69500f145699dedff59488bb29396cf9647
Eli-M-Fisher <eliyahufisher2@gmail.com>
2025-08-04 14:40:54 +0300

    I finished the functions of, cleaning, columns, and execution

--- src/data_cleaner.py
+++ src/data_cleaner.py
@@ -10,13 +10,26 @@ class DataCleaner:
         self.df = df.copy()
 
 # clears a single text
-    def clean_text(self):
+    def clean_text(self, text: str) -> str:
+
+        if not isinstance(text, str):
+            return ""
+        
+        # Remove punctuation
+        text = re.sub(f"[{re.escape(string.punctuation)}]", "", text)
+        # Convert to lowercase
+        text = text.lower()
+        return text
         
 # Removes rows that do not have valid values in the Biased column
     def remove_unspecified_rows(self) -> pd.DataFrame:
 
+        return self.df[self.df["Biased"].isin([0, 1])]
+
+
 # Central all cleaning steps: filters rows, cleans text, and returns a clean table
     def clean_dataset(self) -> pd.DataFrame:
 
-
-        return
+        df_filtered = self.remove_unspecified_rows()
+        df_filtered["Text"] = df_filtered["Text"].apply(self.clean_text)
+        return df_filtered[["Text", "Biased"]].reset_index(drop=True)
